6af94e1dbb4fe0fed970cb0b5962939b633f249f
cartig06 <cartig06660@gmail.com>
2022-01-27 19:54:38 -0500

    Added Project Files

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog("Enter your name");
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+		JOptionPane.showMessageDialog(null, "Hello, "+name+"! You are "+age+" years old!");
+		
+	}
+
+}
